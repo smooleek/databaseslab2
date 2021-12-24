73df580214471d4e2506a8c7b2253ecc9b5dec75
smooleek <73742850+smooleek@users.noreply.github.com>
2021-12-24 18:53:31 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
-SELECT name, cores FROM Specifications INNER JOIN Processors ON Processors.Processor_id = Specifications.Processor_id
+SELECT name, cores FROM Specifications INNER JOIN Processors ON Processors.specifications_id = Specifications.specifications_id
 
 SELECT DISTINCT product_line, count(*) FROM Processors GROUP BY Processors.Product_line
 
-SELECT name, cores, threads from Specifications INNER JOIN info ON info.info = specifications. processor_id INNER JOIN Processors ON Processors.processor_id = specifications. processor_id WHERE vertical_segment = 'Boxed Processor'
+SELECT name, cores, threads from Specifications INNER JOIN info ON info.info = specifications.specifications_id INNER JOIN Processors ON Processors.specifications_id = specifications.specifications_id WHERE vertical_segment = 'Boxed Processor'
