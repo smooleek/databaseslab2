b3b2fcc371dac9920d8beacccff439d136e2e86d
smooleek <73742850+smooleek@users.noreply.github.com>
2021-12-24 18:51:57 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -21,13 +21,13 @@ INSERT INTO info (url, vertical_segment)
 INSERT INTO info (url, vertical_segment)
   	VALUES ('https://www.amd.com/en/product/10471', 'Boxed Processor');
 -- Processors
-INSERT INTO Processors (name, launch_date, product_line, processor_id, info)
+INSERT INTO Processors (name, launch_date, product_line, specifications_id, info)
     VALUES ('AMD Ryzen 9 5950X', '2020-11-05', 'AMD Ryzen 9 Desktop Processors', '1', '1');
-INSERT INTO Processors (name, launch_date, product_line, processor_id, info)
+INSERT INTO Processors (name, launch_date, product_line, specifications_id, info)
     VALUES ('AMD Ryzen 9 5900X', '2020-11-05', 'AMD Ryzen 9 Desktop Processors', '2', '2');
-INSERT INTO Processors (name, launch_date, product_line, processor_id, info)
+INSERT INTO Processors (name, launch_date, product_line, specifications_id, info)
     VALUES ('AMD Ryzen 7 5800X', '2021-01-12', 'AMD Ryzen 7 Desktop Processors', '3', '3');
-INSERT INTO Processors (name, launch_date, product_line, processor_id, info)
+INSERT INTO Processors (name, launch_date, product_line, specifications_id, info)
     VALUES ('AMD Ryzen 7 5800 OEM Only', '2020-07-15', 'AMD Ryzen 7 Desktop Processors', '4', '4');
-INSERT INTO Processors (name, launch_date, product_line, processor_id, info)
+INSERT INTO Processors (name, launch_date, product_line, specifications_id, info)
     VALUES ('AMD Ryzen 5 5600X', '2020-07-15', 'AMD Ryzen 5 Desktop Processors', '5', '5');
