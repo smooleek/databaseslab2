329c0728aba62a914d335876689aefcdc4947067
smooleek <73742850+smooleek@users.noreply.github.com>
2021-12-24 18:51:06 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Info (
 	info SERIAL,
 	url varchar(50) NOT NULL,
-    vertical_segment varchar(20) NOT NULL,
+    	vertical_segment varchar(20) NOT NULL,
 	PRIMARY KEY (info)
 );
 
 CREATE TABLE Specifications (
-	processor_id SERIAL,
+	specifications_id SERIAL,
 	cores SERIAL,
-    threads SERIAL
-    base_frequency SERIAL
-	PRIMARY KEY (period_id)
+    	threads SERIAL,
+    	base_frequency SERIAL,
+	PRIMARY KEY (specifications_id)
 );
 
 CREATE TABLE Processors (
@@ -18,11 +18,11 @@ CREATE TABLE Processors (
 	name VARCHAR(40) NOT NULL,
 	launch_date VARCHAR(10) NOT NULL,
 	info INT NOT NULL,
-	processor_id INT NOT NULL,
-	PRIMARY KEY (processor_id),
+	specifications_id INT NOT NULL,
+	PRIMARY KEY (specifications_id),
 	CONSTRAINT FK_Specifications
-		FOREIGN KEY (processor_id)
-			REFERENCES Specifications (processor_id) ON DELETE CASCADE,
+		FOREIGN KEY (specifications_id)
+			REFERENCES Specifications (specifications_id) ON DELETE CASCADE,
 	CONSTRAINT FK_info
 		FOREIGN KEY (info)
 	  		REFERENCES info (info) ON DELETE CASCADE
